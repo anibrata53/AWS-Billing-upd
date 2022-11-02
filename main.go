b2abdf8aeabbfb8c6a8a3ffa42cd7a8fda11fb7d
anibrata <malkhandi.anibrata@tftus.com>
2022-11-02 18:25:56 +0530

    first commit

--- main.go
+++ main.go
@@ -1,7 +1,36 @@
 package main
 
-import "awscostapi/CreateFile"
+import (
+	"awscostapi/CreateFile"
+	"log"
+	"net/smtp"
+)
 
 func main() {
 	CreateFile.Createfile()
+	SendMail()
+}
+
+// Sending Email Using Smtp in Golang
+
+func SendMail() {
+
+	
+
+	from := "example@gmail.com"
+	password := "example@Password"
+	to := []string{"malkhandi.anibrata@example.com"}
+	smtpHost := "smtp.gmail.com"
+	smtpPort := "465"
+
+	message := []byte("My super secret message.")
+
+	// Create authentication
+	auth := smtp.PlainAuth("", from, password, smtpHost)
+
+	// Send actual message
+	err := smtp.SendMail(smtpHost+":"+smtpPort, auth, from, to, message)
+	if err != nil {
+		log.Fatal(err)
+	}
 }
