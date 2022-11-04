0ea334acfc7a601f5e03b42e45a02f91e68fdedf
anibrata <malkhandi.anibrata@tftus.com>
2022-11-04 11:31:55 +0530

    update

--- main.go
+++ main.go
@@ -2,12 +2,16 @@ package main
 
 import (
 	CSVFile "awscostapi/CSVHandle"
+	"awscostapi/bucket"
 	"awscostapi/mail"
 )
 
 func main() {
 	CSVFile.Createfile()
+	
 	mail.SendMail()
+	bucket.InitBucket()
+
 }
 
 // Sending Email Using Smtp in Golang
